9065af8bac96a241811d800cdf1f26f223ffcd2f
dOcOb <abg1418@gmail.com>
2023-02-23 19:29:08 +0900

    [Feat] programemrs Num42840

---
+++ Java/CodingTest/src/main/java/programmers/lv1/Num42840.java
@@ -0,0 +1,53 @@
+package programmers.lv1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Num42840 {
+    public List<Integer> solution(int[] answers) {
+
+        int[][] players = new int[][] {
+                {1,2,3,4,5},
+                {2,1,2,3,2,4,2,5},
+                {3,3,1,1,2,2,4,4,5,5}
+        };
+
+        int[] results = new int[3];
+
+        int max = 0;
+
+        for(int i = 0; i < 3; i++){
+            int[] player = players[i];
+
+            int j = 0;
+            int cnt = 0;
+
+            for(int answer : answers){
+                if(answer == player[j]){
+                    cnt++;
+                }
+
+                if(player.length == ++j){
+                    j = 0;
+                }
+            }
+
+            if(cnt > max){
+                max = cnt;
+            }
+
+            results[i] = cnt;
+        }
+
+        List<Integer> answer = new ArrayList<>();
+
+        for(int i = 0; i < 3; i++){
+            if(results[i] == max){
+                answer.add(i+1);
+            }
+        }
+
+        return answer;
+    }
+}
