db62137fe7746de2243645bbad065d745ecc8ce6
dOcOb <abg1418@gmail.com>
2023-02-23 16:32:08 +0900

    [Feat] programmers Num43842

---
+++ Java/CodingTest/src/main/java/programmers/lv2/Num42842.java
@@ -0,0 +1,24 @@
+package programmers.lv2;
+
+public class Num42842 {
+    public int[] solution(int brown, int yellow) {
+        int yX, yY, bX, bY;
+        yX = yY = bX = bY = 0;
+
+        brown = brown + yellow;
+
+        for(yX = 1; yX <= yellow / 2 + 1; yX++){
+            bX = yX + 2;
+            if((yellow % yX == 0) && (brown % (bX)) == 0) {
+                yY = yellow / yX;
+                bY = brown / bX;
+                if(yY == (bY - 2)){
+                    break;
+                }
+            }
+        }
+
+        return new int[] {bY, bX};
+    }
+
+}
